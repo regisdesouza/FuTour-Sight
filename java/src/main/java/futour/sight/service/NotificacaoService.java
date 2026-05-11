80ee53a3fcfe4b8351d1ba4d7c53d452e0280905
regisdesouza <reginaldo.lima@sptech.school>
2026-05-11 20:27:14 -0300

    featchoreRetira métodos do Service do Slack que não vamos mais utilizar

--- java/src/main/java/futour/sight/service/NotificacaoService.java
+++ java/src/main/java/futour/sight/service/NotificacaoService.java
@@ -9,26 +9,6 @@ public class NotificacaoService {
 
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
 
-    public static void notificarNovaSolicitacao(String nomeEmpresa, String responsavel, String cnpj, String email) {
-        String mensagem = String.format("""
-                📋 *Nova Solicitação de Cadastro*
-                
-                *Empresa:* %s
-                *Responsável:* %s
-                *CNPJ:* %s
-                *Email:* %s
-                *Data:* %s
-                """,
-                nomeEmpresa,
-                responsavel,
-                formatarCNPJ(cnpj),
-                email,
-                LocalDateTime.now().format(FORMATTER)
-        );
-
-        SlackConfig.enviarMensagemFormatada("Nova Solicitação", mensagem, "good");
-    }
-
     public static void notificarEtlSucesso(String tabela, int registros, long tempoSegundos) {
         String mensagem = String.format("""
                 *Tabela:* %s
@@ -61,30 +41,5 @@ public class NotificacaoService {
         SlackConfig.enviarMensagemFormatada("ETL Falhou", mensagem, "danger");
     }
 
-    public static void notificarNovaEmpresaAprovada(String nomeEmpresa, String cnpj) {
-        String mensagem = String.format("""
-                🎉 *Nova Empresa Ativada*
-                
-                *Empresa:* %s
-                *CNPJ:* %s
-                *Data:* %s
-                """,
-                nomeEmpresa,
-                formatarCNPJ(cnpj),
-                LocalDateTime.now().format(FORMATTER)
-        );
 
-        SlackConfig.enviarMensagemFormatada("Nova Empresa", mensagem, "good");
-    }
-
-    private static String formatarCNPJ(String cnpj) {
-        if (cnpj == null || cnpj.length() != 14) return cnpj;
-        return String.format("%s.%s.%s/%s-%s",
-                cnpj.substring(0, 2),
-                cnpj.substring(2, 5),
-                cnpj.substring(5, 8),
-                cnpj.substring(8, 12),
-                cnpj.substring(12, 14)
-        );
-    }
 }
