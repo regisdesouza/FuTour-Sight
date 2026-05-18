32aa10d943d25aaeb811476a4bf0461e5de0eb67
regisdesouza <reginaldo.lima@sptech.school>
2026-05-17 22:41:56 -0300

    refactor: refatora Classe ChegadaTuristaDAO adaptada para herdar de BaseDAO

--- java/src/main/java/futour/sight/etl/turistas/dao/ChegadaTuristaDAO.java
+++ java/src/main/java/futour/sight/etl/turistas/dao/ChegadaTuristaDAO.java
@@ -1,31 +1,26 @@
 package futour.sight.etl.turistas.dao;
 
+import futour.sight.dao.BaseDAO;
 import futour.sight.etl.turistas.dto.ChegadaTuristaDTO;
 import org.springframework.jdbc.core.JdbcTemplate;
-
 import java.util.List;
 
-public class ChegadaTuristaDAO {
+public class ChegadaTuristaDAO extends BaseDAO {
 
-    private static final int TAMANHO_LOTE = 500;
-
-    private JdbcTemplate jdbc;
+    private static final int TAMANHO_LOTE = 1000;
 
     public ChegadaTuristaDAO(JdbcTemplate jdbc) {
-        this.jdbc = jdbc;
+        super(jdbc);
     }
 
     public void salvarBatch(List<ChegadaTuristaDTO> lista) {
-
         String sql = """
                 INSERT INTO chegadas_turistas
                 (via_de_acesso, uf, nome_pais_origem, mes, ano, chegadas)
                 VALUES (?, ?, ?, ?, ?, ?)
                 """;
-
         for (int i = 0; i < lista.size(); i += TAMANHO_LOTE) {
             List<ChegadaTuristaDTO> lote = lista.subList(i, Math.min(i + TAMANHO_LOTE, lista.size()));
-
             jdbc.batchUpdate(sql, lote, lote.size(), (ps, item) -> {
                 ps.setString(1, item.getViaDeAcesso());
                 ps.setString(2, item.getUf());
