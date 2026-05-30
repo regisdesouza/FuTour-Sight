4b965e62fcf13841dd20f86b05244ecd5c30f4f6
lucas-eiki <lucas.gushiken@sptech.school>
2026-05-30 00:26:49 -0300

    feat: adiciona mensagem de erro caso o ETL falhe ao rodar o run_etl.sh

--- infra/run_etl.sh
+++ infra/run_etl.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-set -e
 
 echo "Executando ETL..."
-sudo docker start ContainerETL
 
-echo "ETL executado!"
+sudo docker start ContainerETL > /dev/null
+
+COD_SAIDA=$(sudo docker wait ContainerETL)
+
+if [ "$COD_SAIDA" -eq 0 ]; then
+    echo "ETL executado com sucesso!"
+
+    else
+        echo "ETL falhou! Código: $COD_SAIDA"
+fi
