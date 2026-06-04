2989dd0b2956b90fcfc0907e15e9af42d5c9d0ce
lucas-eiki <lucas.gushiken@sptech.school>
2026-06-04 15:56:11 -0300

    feat: adiciona script de crairRoles.sh

---
+++ infra/criarRoles.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+echo "Criando grupos..."
+
+echo "Criando grupo infra..."
+sudo groupadd infra
+
+echo "Criando grupo dev..."
+sudo groupadd dev
+
+echo "Criando usuários..."
+
+echo "Criando usuário: eiki"
+sudo adduser eiki
+
+echo "Criando usuário: frossi"
+sudo adduser frossi
+
+echo "Criando usuário: reginaldo"
+sudo adduser reginaldo
+
+echo "Criando usuário: debora"
+sudo adduser debora
+
+echo "Criando usuário: gabriel"
+sudo adduser gabriel
+
+echo "Adicionando usuários aos grupos..."
+sudo usermod -aG infra eiki
+sudo usermod -aG infra gabriel
+
+sudo usermod -aG dev reginaldo
+sudo usermod -aG dev frossi
+sudo usermod -aG dev eiki
+
+sudo usermod -aG sudo debora
+
+echo "Roles criadas com sucesso!"
