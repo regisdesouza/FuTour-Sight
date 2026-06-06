d5358b740ed4f2fc064985b0c3b137a207b97e30
lucas-eiki <lucas.gushiken@sptech.school>
2026-06-05 22:23:28 -0300

    feat: muda a origem dinâmica ao copiar o repositório para sempre ser na /home/ubuntu

--- infra/setup.sh
+++ infra/setup.sh
@@ -38,7 +38,7 @@ fi
 sudo mkdir -p "$DIR_BASE"
 
 echo "Copiando repositório..."
-sudo rsync -a --exclude='node_modules/' --delete "$HOME/FuTour-Sight/." "$DIR_REPO"
+sudo rsync -a --exclude='node_modules/' --delete "/home/ubuntu/FuTour-Sight/." "$DIR_REPO"
 
 echo "Copiando Docker Compose..."
 sudo cp "$DIR_REPO/infra/dockers/docker-compose.yml" "$DIR_BASE/docker-compose.yml"
