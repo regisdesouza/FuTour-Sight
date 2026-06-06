17c5b301370bc1dbc151424c66db32585aa9dc24
lucas-eiki <lucas.gushiken@sptech.school>
2026-06-06 03:01:56 -0300

    fix: diminui permissões de outros, permite o acesso ao usuario mysql e configura permissao de arquivos restantes

--- infra/configurarPermissoes.sh
+++ infra/configurarPermissoes.sh
@@ -8,12 +8,21 @@ DIR_BASE="/opt/futour-sight"
 DIR_REPO="${DIR_BASE}/repo"
 
 sudo chown root:futour $DIR_BASE
-sudo chmod 754 $DIR_BASE
+sudo chmod 750 $DIR_BASE
+
+sudo chown -R root:futour "$DIR_REPO"
+sudo chmod -R 750 "$DIR_REPO"
 
 sudo chown -R root:infra "$DIR_REPO/infra"
-sudo chmod -R 754 "$DIR_REPO/infra"
+sudo chmod -R 750 "$DIR_REPO/infra"
 
 sudo chown -R root:dev "$DIR_REPO/FuTour-Sight"
-sudo chmod -R 754 "$DIR_REPO/FuTour-Sight"
+sudo chmod -R 750 "$DIR_REPO/FuTour-Sight"
+
+sudo chown -R root:dev "$DIR_REPO/java"
+sudo chmod -R 750 "$DIR_REPO/java"
+
+sudo chmod 644 "$DIR_REPO/infra/mysql.cnf"
+sudo chmod 644 "$DIR_REPO/FuTour-Sight/src/database/bd-futour-sight.sql"
 
 echo "Permissões configuradas com sucesso!"
