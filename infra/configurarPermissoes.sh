9503be885d1e2bdfcfbfae87415ff856c5e195c3
lucas-eiki <lucas.gushiken@sptech.school>
2026-06-06 02:19:15 -0300

    fix: modifica permissões para permitir acesso ao mysql

--- infra/configurarPermissoes.sh
+++ infra/configurarPermissoes.sh
@@ -8,12 +8,12 @@ DIR_BASE="/opt/futour-sight"
 DIR_REPO="${DIR_BASE}/repo"
 
 sudo chown root:futour $DIR_BASE
-sudo chmod 750 $DIR_BASE
+sudo chmod 754 $DIR_BASE
 
 sudo chown -R root:infra "$DIR_REPO/infra"
-sudo chmod -R 750 "$DIR_REPO/infra"
+sudo chmod -R 754 "$DIR_REPO/infra"
 
 sudo chown -R root:dev "$DIR_REPO/FuTour-Sight"
-sudo chmod -R 750 "$DIR_REPO/FuTour-Sight"
+sudo chmod -R 754 "$DIR_REPO/FuTour-Sight"
 
 echo "Permissões configuradas com sucesso!"
