3f4bbcc45a3ba253b9b0a7c1d5e027c0b7de3af6
lucas-eiki <lucas.gushiken@sptech.school>
2026-06-06 00:02:12 -0300

    feat: remove sudo do run_etl e deploy

--- infra/run_etl.sh
+++ infra/run_etl.sh
@@ -2,9 +2,9 @@
 
 echo "Executando ETL..."
 
-sudo docker start ContainerETL > /dev/null
+docker start ContainerETL > /dev/null
 
-COD_SAIDA=$(sudo docker wait ContainerETL)
+COD_SAIDA=$(docker wait ContainerETL)
 
 if [ "$COD_SAIDA" -eq 0 ]; then
     echo "ETL executado com sucesso!"
